e6e87bc984b5085cd12f967828625379406f6cc8
Natalie Gillam <ngillam@hotmail.co.uk>
2023-12-31 18:23:07 +0000

    Created an option element for each available voice in the Text to Speech API

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//Initialise SppechSythesis API
+const synth = window.speechSynthesis;
+
+//Fetching DOM elements
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const voiceSelect = document.querySelector("#voice-select");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector("#rate-value");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector("#pitch-value");
+
+//Initialise the voices array
+let voices = [];
+
+const getVoices = () => {
+  voices = synth.getVoices();
+  //Loop through voices and create an option for each one
+  voices.forEach((voice) => {
+    //Create option element for each voice
+    const option = document.createElement("option");
+    //Fill option with voice and language
+    option.textContent = voice.lang + " (" + voice.name + ")";
+    //Set required option attributes
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    voiceSelect.appendChild(option);
+  });
+};
+
+getVoices();
+
+//the following code is required in order to get the voices.
+if (synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = getVoices;
+}
